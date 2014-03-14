dc0d33507624c7a3132bd6fc3395dc1a3d8f63b0
Lei Wang <LeiWang2013@u.northwestern.edu>
2014-03-14 11:12:02 -0500

    Now start working on user level functions, will add sector defination to image-more.rkt

--- instruction.rkt
+++ instruction.rkt
@@ -6,7 +6,7 @@ In the file mrlib/image-core.rkt search for "a shape is either ...." and the nex
 
 Add 'sector' to the np-atomic-shape definition. (Pattern it after ellipse.)
 
-Add sector cases to all of the functions in image-core that accept np-atomic-polygons: make them all signal errors, at first.
+Add sector cases to all of the functions in image-core that accept np-atomic-shapes(polygons): make them all signal errors, at first.
 
 The user-level functions are in 2htdp/private/image-more.rkt; add a 'sector' function there that is like 'ellipse' or 'circle'. Use define/chk.
 
